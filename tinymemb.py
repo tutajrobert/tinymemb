13b6c3a71eedd7206af6030185133e4f977b418f
tutaj robert <tutajrobert@gmail.com>
2019-03-06 23:25:05 +0100

    Code reorganization. Geom, Prep, Mesh OK
    Next step: solver. Preintegrated matrix of the element. Gauss elimination method.

--- tinymemb.py
+++ tinymemb.py
@@ -4,9 +4,6 @@ class Prep():
         self.edict = {}
         self.nnum = 0
         self.enum = 0
-        self.meshnum = 0
-        self.meshdict = {}
-        self.next_nnum = 1
 
     def node(self, x, y):
         self.nnum += 1
@@ -28,36 +25,7 @@ class Prep():
         self.edict[self.enum] = [n1, n2, n3, n4]
         return self.enum
 
-
-    def mesh(self, supele, size, property):
-        self.meshnum += 1
-        nodes = []
-        eles = []
-
-        start, end = supele[0], supele[1]
-        xlist, ylist = [start[0], end[0]], [start[1], end[1]]
-        xmin, xmax = min(xlist), max(xlist)
-        ymin, ymax = min(ylist), max(ylist)
-        xlen = abs(end[0] - start[0])
-        ylen = abs(end[1] - start[1])
-
-        for i in range(xmin, xmax + 1, size):
-            for j in range(ymin, ymax + 1, size):
-                nodes.append(self.node(i, j))
-
-        for i in range(xlen):
-            for j in range(self.next_nnum, self.next_nnum + ylen):
-                eles.append(self.ele(j + 1 + ((ylen + 1) * i), j + 1 + ((ylen + 1) * (i + 1)),
-                                     j + ((ylen + 1) * (i + 1)), j + ((ylen + 1) * i)))
-
-        self.meshdict[self.meshnum] = [eles]
-        self.next_nnum += 1 + nodes[-1] - nodes[0]
-        return self.meshnum
-
-    def nmerge(self, x, y):
-        nnum_list = self.ncheck(x, y)
-        if len(nnum_list) > 1:
-            nnum_tomerge = min(nnum_list)
+    def nswap_in_eles(self, nnum_list, nnum_tomerge):
         for enum in self.edict:
             for nnum in nnum_list:
                 if self.edict[enum][0] == nnum:
@@ -68,10 +36,12 @@ class Prep():
                     self.edict[enum][2] = nnum_tomerge
                 elif self.edict[enum][3] == nnum:
                     self.edict[enum][3] = nnum_tomerge
+
+    def nclear_unused(self, nnum_list, nnum_tomerge):
         for nnum in nnum_list:
             if nnum != nnum_tomerge:
                 self.ndel(nnum)
-
+        
 class Geom():
     def __init__(self):
         self.kdict = {}
@@ -82,7 +52,7 @@ class Geom():
     def supele(self, kpoint_start, kpoint_end):
         self.supnum += 1
         self.supdict[self.supnum] = [kpoint_start, kpoint_end]
-        return self.supdict[self.supnum]
+        return self.supnum
 
     def kpoint(self, x, y):
         self.knum += 1
@@ -90,18 +60,18 @@ class Geom():
         return self.kdict[self.knum]
 
 class Solv():
-    def __init__(self, prepclass):
-        self.prepclass = prepclass
+    def __init__(self, meshclass):
+        self.prepclass = meshclass.PREP
+        self.meshclass = meshclass
         nodesnum = len(self.prepclass.ndict)
         self.clist = [0 for i in range(nodesnum * 2)]
         self.gklist = [[0 for i in range(nodesnum * 2)] for j in range(nodesnum * 2)]
 
     @staticmethod
-    def stiff_matrix():
+    def stiff_matrix(E, v, h):
         """Element stiffness matrix"""
-        v = .5
-        p = 1 / (12 * (1 - (v ** 2)))
-        q = 1 / (1 - v)
+        p = (E * h) / (12 * (1 - (v ** 2)))
+        q = (E * h) / (1 - v)
         k11 = p * 2 * (3 - v)
         k15 = p * (-3 + v)
         k12 = q / 8
@@ -110,8 +80,6 @@ class Solv():
         k18 = p * 1.5 * (1 - (3 * v))
         k13 = p * (-3 - v)
         k14 = -k18
-
-        #Element stiffnes matrix
         klist = [[k11, k12, k13, k14, k15, k16, k17, k18],
                  [k12, k11, k18, k17, k16, k15, k14, k13],
                  [k13, k18, k11, k16, k17, k14, k15, k12],
@@ -121,3 +89,59 @@ class Solv():
                  [k17, k14, k15, k12, k13, k18, k11, k16],
                  [k18, k13, k12, k15, k14, k17, k16, k11]]
         return klist
+    
+    def build(self):
+        for meshnum in self.meshclass.meshdict:
+            eles = self.meshclass.meshdict[meshnum][0]
+            mat_params = self.meshclass.meshdict[meshnum][1]
+            thickness = self.meshclass.meshdict[meshnum][2]
+            size = self.meshclass.meshdict[meshnum][3]
+            
+            klist = self.stiff_matrix(mat_params[0], mat_params[1], thickness)
+            
+            for ele in eles:
+                pass
+    
+class Mesh():
+    def __init__(self, geomclass):
+        self.geomclass = geomclass
+        self.PREP = Prep()
+        self.meshdict = {}
+        self.meshnum = 0
+        self.next_nnum = 1
+        
+    def generate(self, rectnum, size=1):
+        self.meshnum += 1
+        nodes = []
+        eles = []
+        
+        rectgeom = self.geomclass.supdict[rectnum]
+        xlist, ylist = [rectgeom[0][0], rectgeom[1][0]], [rectgeom[0][1], rectgeom[1][1]]
+        width = abs(rectgeom[1][0] - rectgeom[0][0])
+        height = abs(rectgeom[1][1] - rectgeom[0][1])
+
+        for i in range(min(xlist), max(xlist) + 1, size):
+            for j in range(min(ylist), max(ylist) + 1, size):
+                nodes.append(self.PREP.node(i, j))
+
+        for i in range(width):
+            for j in range(self.next_nnum, self.next_nnum + height):
+                eles.append(self.PREP.ele(j + 1 + ((height + 1) * i), 
+                                          j + 1 + ((height + 1) * (i + 1)),
+                                          j + ((height + 1) * (i + 1)), 
+                                          j + ((height + 1) * i)))
+
+        self.meshdict[self.meshnum] = [eles, 0, 0, size]
+        self.next_nnum += 1 + nodes[-1] - nodes[0]
+        return self.meshnum
+
+    def nmerge(self, x, y):
+        nnum_list = self.PREP.ncheck(x, y)
+        if nnum_list:
+            nnum_tomerge = min(nnum_list)
+            self.PREP.nswap_in_eles(nnum_list, nnum_tomerge)
+            self.PREP.nclear_unused(nnum_list, nnum_tomerge)
+
+    def assignprop(self, meshnum, mat_params, thickness):
+        self.meshdict[meshnum][1] = mat_params
+        self.meshdict[meshnum][2] = thickness
